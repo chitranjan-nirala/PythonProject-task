d0f9be37e631347fd0cb3426c589123005d85a97
chitranjan kumar <chitranjan993483@gmail.com>
2025-03-26 00:01:04 +0530

    first commit

---
+++ task2.py
@@ -0,0 +1,10 @@
+# Problem Statement: Write a Python program that:
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+
+Fname = input("Enter your Firstname:")
+Lname = input("Enter your Lastname:")
+Fullname = Fname+" "+Lname
+
+print("Hello,"+" "+Fullname+"!"+" "+ "welcome to the python program.")
